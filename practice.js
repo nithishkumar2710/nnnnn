00d952ba8a26548f8bb49a20d23328e6a9f1055d
nithishkumar2710 <143180598+nithishkumar2710@users.noreply.github.com>
2024-05-17 18:46:01 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,95 @@
+//Functions
+
+// function sum(num1,num2) {
+//     return num1+num2;
+// }
+// console.log(sum(10,2));
+// console.log(sum(50,50));
+
+//SLICE
+// function getUserNameFromEmail(email){
+//     return(email.slice(2,email.indexOf("g")));
+// }
+
+// console.log(getUserNameFromEmail
+// ("contactnithish@gmail.com"));
+
+// //first letter caps
+// function toProperCase(name){
+//     return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+// }
+// console.log(toProperCase("nitHISHTENDulkar"));
+
+function greet(Hello){
+  
+}
+ greet("Alice"); 
+
+//  //declaring a function
+//  function greet(name) {
+//      console.log("Hello, " + name + "!");
+//  }
+
+// // // //calling a function
+// greet("Nithish");
+
+// // // //returning a value
+// function add(a, b) {
+//   return a + b;
+//  }
+//  console.log(add(5, 2));
+
+// //variable hoisting
+//  console.log(x); 
+//  var x = 5;
+
+// // function hoisting
+//  greet("John"); 
+//  function greet(name) {
+//    console.log("Hello, " + name + "!");
+//  }
+
+// //function arguments
+// function greet(name) {
+//   console.log("Hello, " + name + "!");
+// }
+// greet("Sam"); 
+
+// //scope and environment
+// function scope() {
+//   var x = 10;
+//   console.log(x); 
+// }
+
+// scope();
+// console.log(x); 
+
+// // var x = 20;
+// // console.log(x);
+
+// //anonymous function
+
+// var add = function (a, b) {
+//   return a + b;
+// }
+
+// console.log(add(2, 3));
+
+// //recursive function
+// function factorial(n) {
+//   if (n == 0) {
+//     return 1;
+//   }
+//   else {
+//     return n * factorial(n - 1);
+//   }
+// }
+// console.log(factorial(7));
+
+// //default parameters
+// function greet(name = "World") {
+//   console.log("Hii, " + name + "!");
+// }
+
+// greet(); 
+// greet("Mano");
